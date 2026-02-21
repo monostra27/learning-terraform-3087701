d6044bdbbc8bb0089b1136f5d695cf3382267d5d
Anthony Monostra <40921326+monostra27@users.noreply.github.com>
2026-02-21 18:27:33 -0500

    Uncomment instance_type variable definition

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t3.nano"
-#}
+variable "instance_type" {
+ description = "Type of EC2 instance to provision"
+ default     = "t3.nano"
+}
 variable "AWS_SECRET_ACCESS_KEY" {
   description = "The AWS secret access key"
   type        = string
