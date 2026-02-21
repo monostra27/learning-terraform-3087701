ba4d1039a78ed08a00819735be08fc6d3ef061f0
Anthony Monostra <40921326+monostra27@users.noreply.github.com>
2026-02-21 16:38:41 -0500

    Change instance type from t2.micro to t3.micro

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.micro" ##t 2.micro  ##"t3.nano"
+  instance_type = "t3.micro" ##t 2.micro  ##"t3.nano"
 
   tags = {
     Name = "HelloWorld"
