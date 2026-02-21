9144759d7b34fcbcd0f00b1b0c58a342a5ef9eb3
Anthony Monostra <40921326+monostra27@users.noreply.github.com>
2026-02-21 15:07:58 -0500

    Define AWS_SECRET_ACCESS_KEY variable
    Added AWS_SECRET_ACCESS_KEY variable with sensitive flag.

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The AWS secret access key"
+  type        = GDmJWCsAd+IkAveKpQyL5Kx/Tvcws+6pvZuUS6Bx
+  sensitive   = true # Mark as sensitive to prevent display in logs
+}
