fdfce8f80143c36532e0b8c6b76fb87e161a5b40
Anthony Monostra <40921326+monostra27@users.noreply.github.com>
2026-02-22 12:11:05 -0500

    Fix typos in aws_security_group_rule resources
    Corrected typos in resource names for security group rules.

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_secuirty_group" "blob" {
   vpc_id = data.aws_vpc.defaukt.id
 }
 
-resource "aws_secuirty_group_rule" "blob_http_in" {
+resource "aws_security_group_rule" "blob_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -46,7 +46,7 @@ resource "aws_secuirty_group_rule" "blob_http_in" {
   security_grooup_id = aws_security_group.blob.id
 }
 
-resource "aws_secuirty_group_rule" "blob_https_in" {
+resource "aws_security_group_rule" "blob_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
@@ -56,7 +56,7 @@ resource "aws_secuirty_group_rule" "blob_https_in" {
   security_grooup_id = aws_security_group.blob.id
 }
 
-resource "aws_secuirty_group_rule" "blob_everthing_out" {
+resource "aws_security_group_rule" "blob_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
