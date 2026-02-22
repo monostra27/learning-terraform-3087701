bcc91a4a35798b52ee23309afd1049c10f058e39
Anthony Monostra <40921326+monostra27@users.noreply.github.com>
2026-02-22 13:43:41 -0500

    Refactor AWS instance security group configuration
    Updated security group reference and added new module for security group management.

--- main.tf
+++ main.tf
@@ -22,12 +22,27 @@ resource "aws_instance" "blob" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type   ##t 2.micro  ##"t3.nano" t3.micro"
 
-  vpc_security_group_ids = [aws_security_group.blob.id]
+  vpc_security_group_ids = [module.blob.sg.security_group_id]  ##aws_security_group.blob.id
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+module "blob_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.1"
+  name = "blob_new"
+
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cird_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cird_blocks = ["0.0.0.0/0"]
+}
+
 
 resource "aws_security_group" "blob" {
   name = "blob"
