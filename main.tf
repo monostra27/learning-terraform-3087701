f04b34820d760bef6882c018f7e4e337bd752d05
Anthony Monostra <40921326+monostra27@users.noreply.github.com>
2026-02-22 18:09:36 -0500

    Remove blob_vpc module configuration

--- main.tf
+++ main.tf
@@ -45,16 +45,3 @@ resource "aws_instance" "blob" {
   }
 }
 
-module "blob_vpc" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.1"
-  name = "blob"
-
-  vpc_id = module.blob_vpc.vpc_id
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
