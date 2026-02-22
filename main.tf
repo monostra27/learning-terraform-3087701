fed5bd0c8f5826f999a9c361a834de37e553a550
Anthony Monostra <40921326+monostra27@users.noreply.github.com>
2026-02-22 16:01:09 -0500

    Add VPC module and update security group references

--- main.tf
+++ main.tf
@@ -14,8 +14,22 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc_blob" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blob" {
@@ -23,6 +37,8 @@ resource "aws_instance" "blob" {
   instance_type = var.instance_type   ##t 2.micro  ##"t3.nano" t3.micro"
 
   vpc_security_group_ids = [module.blob_sg.security_group_id]  ##aws_security_group.blob.id
+
+  subnet_id = module.blob_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -34,7 +50,7 @@ module "blob_sg" {
   version = "5.3.1"
   name = "blob"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blob_vcp.vcp.id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
