e3b4cb61b84588521258c72e3848d1bf676425db
Anthony Monostra <40921326+monostra27@users.noreply.github.com>
2026-02-23 19:37:38 -0500

    Refactor Terraform configuration for blob ALB setup

--- main.tf
+++ main.tf
@@ -14,34 +14,39 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "blob_vpc" {
+module "blob_alb" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = "blob_alb"
+  vpc_id = module.blob_vpc.vpc_id
+  subnets = module.blob_vpc.public_subnets[0]
+  
+  security_groups = {module.blob_sg.security_group_id}
 
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
+  listeners = {
+    blob-http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_arn = aws_lb_target_group.blob.arn
+      }
+    }
   
   tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+    Environment = "Development"
+    }
 }
 
-resource "aws_instance" "blob" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type   ##t 2.micro  ##"t3.nano" t3.micro"
+resource "aws_lb_target_group" "blob {
+  name     = "blob"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.blob_vpc.vpc_id
+}
 
-  #vpc_security_group_ids = [module.blob_vpc.security_group_id]  ##aws_security_group.blob.id
-
-  subnet_id = module.blob_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
+resource "aws_lb_target_group_attachment" "blob" {
+  target_group_arn = aws_lb_target_group.blob.arn
+  target_id        = aws_instance.blob.id
+  port             = 80
 }
 
